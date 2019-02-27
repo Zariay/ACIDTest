3a3769624a3649bebe0a719e3d57436d6a5b9b2b
Zariay <alexswfan@gmail.com>
2019-02-26 18:38:21 -0800

    data.js

---
+++ backend/data.js
@@ -0,0 +1,18 @@
+// JavaScript source code
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const DataSchema = new Schema(
+    {
+        id: Number,
+        x: Number,
+        y: Number,
+        z: Number,
+        r: Number,
+        g: Number,
+        b: Number
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("Data", DataSchema);
