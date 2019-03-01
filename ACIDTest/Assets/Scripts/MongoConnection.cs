c15b7c435acfa5183db3045d90fd6e84112a736e
Zariay <alexswfan@gmail.com>
2019-02-28 22:47:09 -0800

    MongoDB.dll.meta and accessing server

--- ACIDTest/Assets/Scripts/MongoConnection.cs
+++ ACIDTest/Assets/Scripts/MongoConnection.cs
@@ -1,14 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using MongoDB.Driver;
+using MongoDB.Bson;
 
 public class MongoConnection : MonoBehaviour
 {
+    private MongoClient client;
+    private MongoServer server;
+    private MongoDatabase db;
     // Start is called before the first frame update
-    void Start()
+    protected void Start()
     {
-        
+        client = new MongoClient(new MongoUrl("mongodb://localhost:27017"));
+        server = client.GetServer();
+        server.Connect();
+        db = server.GetDatabase("cubeData");
     }
 
     // Update is called once per frame
